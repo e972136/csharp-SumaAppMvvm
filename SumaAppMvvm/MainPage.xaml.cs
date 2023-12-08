3ed337dae4203eb64f1bcf618e1beebc3ee2bae6
Gaspar <e972136@gmail.com>
2023-12-08 14:00:53 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvm/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SumaAppMvvm
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+   
+    }
+
+}
---
+++ SumaAppMvvm/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    internal partial class MainViewModel: ObservableObject 
+    {
+        [ObservableProperty]
+        private String? dato1;
+        
+        [ObservableProperty]
+        private String? dato2;
+        
+        [ObservableProperty]
+        private String? respuesta;
+
+        [RelayCommand]
+        public void Sumar()
+        {
+            if (Dato1 == null || !int.TryParse(Dato1, out int n1))
+            {
+                Respuesta = "Problema Dato 1";
+                return;
+            }
+            if (Dato2 == null || !int.TryParse(Dato2, out int n2))
+            {
+                Respuesta = "Problema Dato 2";
+                return;
+            }
+            Respuesta = (n1 + n2) + "";
+        }
+
+        [RelayCommand]
+        public void LimpiarDatos()
+        {
+            Dato1 = "";
+            Dato2 = "";
+            Respuesta = "";
+        }
+
+    }
+}
